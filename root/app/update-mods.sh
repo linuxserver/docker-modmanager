2b909ece6d15467ef6c954c8528b5a849610b848
thespad <spad@linuxserver.io>
2025-01-05 20:11:36 +0000

    Include context in log output

--- root/app/update-mods.sh
+++ root/app/update-mods.sh
@@ -4,6 +4,7 @@
 find_docker_mods() {
     # Mods provided via Docker
     if [[ "${2}" != "default" ]]; then
+        MOD_STATE="(${2})"
         docker context create "${2}" --docker "host=${1}" >/dev/null 2>&1
     fi
     docker --context "${2}" ps -q >/dev/null 2>&1 || local DOCKER_MOD_CONTEXT_FAIL=true
@@ -11,7 +12,7 @@ find_docker_mods() {
         echo "[mod-init] (ERROR) Cannot connect to the Docker daemon at ${2}, skipping host"
         return
     fi
-    echo -e "[mod-init] Searching all containers in the ${2} context for DOCKER_MODS..."
+    echo -e "[mod-init] ${MOD_STATE:+${MOD_STATE} }Searching all containers in the ${2} context for DOCKER_MODS..."
     for CONTAINER in $(docker --context "${2}" ps -q); do
         CONTAINER_MODS=$(docker --context "${2}"  inspect "${CONTAINER}" | jq -r '.[].Config.Env | to_entries | map(select(.value | match("DOCKER_MODS="))) | .[].value')
         CONTAINER_NAME=$(docker --context "${2}"  inspect "${CONTAINER}" | jq -r .[].Name | cut -d '/' -f2)
@@ -19,9 +20,9 @@ find_docker_mods() {
             CONTAINER_MODS=$(awk -F '=' '{print $2}' <<< "${CONTAINER_MODS}")
             for CONTAINER_MOD in $(tr '|' '\n' <<< "${CONTAINER_MODS}"); do
                 if [[ "${DOCKER_MODS}" =~ ${CONTAINER_MOD} ]]; then
-                    echo -e "[mod-init] ${CONTAINER_MOD} already in mod list, skipping"
+                    echo -e "[mod-init] ${MOD_STATE:+${MOD_STATE} }${CONTAINER_MOD} already in mod list, skipping"
                 else
-                    echo -e "[mod-init] Found new mod ${CONTAINER_MOD} for container ${CONTAINER_NAME}"
+                    echo -e "[mod-init] ${MOD_STATE:+${MOD_STATE} }Found new mod ${CONTAINER_MOD} for container ${CONTAINER_NAME}"
                     DOCKER_MODS="${DOCKER_MODS}|${CONTAINER_MOD}"
                     DOCKER_MODS="${DOCKER_MODS#|}"
                 fi
